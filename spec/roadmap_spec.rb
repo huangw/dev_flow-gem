b7a1608ad074bb8d1f0d9c2c9917f89783c42d70
Huang Wei <huangw@pe-po.com>
2013-03-21 18:27:05 +0800

    test and implemented task adn roadmap.

--- spec/roadmap_spec.rb
+++ spec/roadmap_spec.rb
@@ -10,6 +10,17 @@ describe DevFlow::RoadMap do
       it "should be a Roadmap object" do
         roadmap.is_a?(DevFlow::RoadMap).should be_true
       end
+
+      it "should assign parents well" do
+        roadmap.ln_tasks[23].parent.is_a?(DevFlow::Task)
+        roadmap.ln_tasks[23].parent.branch_name.should eq("scope")
+      end
+
+      it "should assign dependencies well" do
+        roadmap.ln_tasks[64].dependencies.size.should eq(2)
+        roadmap.ln_tasks[64].dependencies[0].branch_name.should eq("release_api_design_0.1")
+        roadmap.ln_tasks[64].dependencies[1].branch_name.should eq("model_spec")
+      end
     end
   end
 end
