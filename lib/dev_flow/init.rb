cba89ed15812383a9b40e845b46d27bc6a5fc24d
Huang Wei <huangw@pe-po.com>
2013-03-21 15:25:16 +0100

    add default remote git server to local configuration.

--- lib/dev_flow/init.rb
+++ lib/dev_flow/init.rb
@@ -2,6 +2,8 @@ module DevFlow
   class Init < App
 
     def process!
+
+      # find the current user
       raise "No known members defined!" unless all_member_names.size > 0
         
       sugguest = @config["whoami"] if @config["whoami"]
@@ -13,30 +15,44 @@ module DevFlow
         sugguest = @girc.config["user.email"].gsub(/\@.+$/, '') if @girc.config["user.email"]     
       end
       
-      puts "Known members: " + all_member_names.join(", ")
-      msg = "Tell us who you are: "
+      puts "Tell us who you are: ".bold.yellow
+      msg = all_member_names.join(", ")
       if all_member_names.include? sugguest
-        msg += "[#{sugguest}]"
+        msg += " [#{sugguest}]"
       end
 
-      print msg
+      print msg + ":"
       ans = STDIN.gets.chomp!
 
       ans = sugguest unless ans.size > 0
       raise "Unknown member!" unless all_member_names.include? ans
 
-      write_local_config({"whoami" => ans})
-      add_to_gitignore()
-
+      # find the default git remote server
       @config["whoami"] = ans
       puts "Welcome #{self.user.bold}!"
+
+      remotes = @girc.remote_list
+      raise "You need to set at least one remote git server to interact with!" unless remotes.size > 0
+
+      puts "Which remote git server to use?".bold.yellow
+      msg = remotes.join ", "
+      
+      suggest = @config["git_remote"] || 'origin'
+      msg += " [#{suggest}]" if remotes.include? suggest
+      print msg + ":"
+
+      ans2 = STDIN.gets.chomp!
+      ans2 = suggest unless ans2.size > 0
+      raise "You must define a valid git remote server" unless remotes.include? ans2
+
+      # write out to the local configuration file
+      write_local_config({"whoami" => ans, "git_remote" => ans2})
+      add_to_gitignore()
     end
 
     def write_local_config hash
       wh = File.open(@config[:local_config], 'w:utf-8')
-      hash.each do |k,v|
-        wh.puts "#{k}: #{v}"
-      end
+      YAML.dump(hash, wh)
       wh.close
     end
 
