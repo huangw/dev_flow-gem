4e26f0c91140c71667fa2d96a15aea4dc5920a66
Huang Wei <huangw@pe-po.com>
2013-03-21 20:24:23 +0800

    complete init.

--- lib/dev_flow/app.rb
+++ lib/dev_flow/app.rb
@@ -1,8 +1,9 @@
 require 'yaml'
 require 'logger'
+class String; include Term::ANSIColor end
 
 module DevFlow
-  attr_accessor :config, :roadmap, :logger, :command
+  attr_accessor :config, :roadmap, :logger, :command, :girc
   class App
     def initialize config, command
       @config, @command = config, command
@@ -20,16 +21,27 @@ module DevFlow
         @config = @roadmap.config
       end
 
+      @girc = Girc::Console.new
+
       @logger = Logger.new(STDOUT)
       @logger.level = Logger::WARN
       @logger.formatter = proc { |severity, datetime, progname, msg|
         "#{msg.to_s}\n"
       }
-
     end
 
     def all_member_names
-      @config["members"].keys
+      @config["members"] ? @config["members"].keys : []
     end
+
+    def user
+      wi = @config["whoami"]
+      if wi and @config["members"] and @config["members"][wi] and @config["members"][wi][0]
+        @config["members"][wi][0]
+      else
+        wi
+      end
+    end
+
   end
 end
--- lib/dev_flow/init.rb
+++ lib/dev_flow/init.rb
@@ -1,8 +1,61 @@
 module DevFlow
   class Init < App
+
     def process!
+      raise "No known members defined!" unless all_member_names.size > 0
+        
+      sugguest = @config["whoami"] if @config["whoami"]
+      begin
+        suggest = `whoami` unless all_member_names.include? sugguest
+      end
+
+      unless all_member_names.include? sugguest
+        sugguest = @girc.config["user.email"].gsub(/\@.+$/, '') if @girc.config["user.email"]     
+      end
       
-      # if @config["whoami"]
+      puts "Known members: " + all_member_names.join(", ")
+      msg = "Tell us who you are: "
+      if all_member_names.include? sugguest
+        msg += "[#{sugguest}]"
+      end
+
+      print msg
+      ans = STDIN.gets.chomp!
+
+      ans = sugguest unless ans.size > 0
+      raise "Unknown member!" unless all_member_names.include? ans
+
+      write_local_config({"whoami" => ans})
+      add_to_gitignore()
+
+      @config["whoami"] = ans
+      puts "Welcome #{self.user.bold}!"
     end
-  end
+
+    def write_local_config hash
+      wh = File.open(@config[:local_config], 'w:utf-8')
+      hash.each do |k,v|
+        wh.puts "#{k}: #{v}"
+      end
+      wh.close
+    end
+
+    def add_to_gitignore
+      had = false
+      if File.exists?(".gitignore")
+        fh = File.open(".gitignore")
+        fh.each do |line|
+          had = true if line =~ /#{@config[:local_config]}/
+        end
+        fh.close
+      end
+      
+      unless had
+        wh = File.open(".gitignore", 'a')
+        wh.puts @config[:local_config]
+        wh.close
+      end
+    end
+
+  end # class
 end
