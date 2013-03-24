3bb08407dce4fc2de452ae9fd5cdf0caec9e7b5a
Huang Wei <huangw@pe-po.com>
2013-03-24 16:45:39 +0100

    debug on progress.

--- lib/dev_flow/commands/ur.rb
+++ lib/dev_flow/commands/ur.rb
@@ -4,7 +4,7 @@ module DevFlow
     def process!
       error "Not on develop trunk" unless @git.current_branch == 'develop'
       error "Only leader/moderator and supervisor can edit ROADMAP" unless i_have_power?
-      p @git.modified_files
+      #p @git.modified_files
       error "No change detected on #{@config[:roadmap]}" unless @git.modified_files.include? File.expand_path(@config[:roadmap])
 
       `git add .`
