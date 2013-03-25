2fca060aa462919ab2b2014d408d220230947e05
Huang Wei <huangw@pe-po.com>
2013-03-25 11:30:34 +0800

    debug for info.

--- lib/dev_flow/commands/info.rb
+++ lib/dev_flow/commands/info.rb
@@ -1,48 +1,6 @@
 module DevFlow
   class Info < App
 
-    def process
-      self.hello
-      self.ask_rebase
-      puts self.hr
-
-      current_task = self.task
-
-      if self.i_am_leader and self.need_to_close.size > 0
-        self.display_close_waiting
-      else
-        self.display_tasks
-      end
-
-      # special treatment for release branches
-      if current_task and current_task.is_release
-        if self.i_am_leader
-          puts "You are on a release branch".bold.red
-          puts "You may should release it before swtitch to other tasks.".bold.red
-        else
-          puts "You do not have the permission to release this branch".bold.red
-          if wd_clean? 
-            puts "Please switch to other branches".bold.red
-          else
-            puts "Please undo you edition and switch to other branches".bold.red
-          end
-        end
-        puts hrb
-      elsif current_task and current_task.progress == 99 and self.i_am_leader
-        puts "You may want to check and review the code and issue "
-        puts "    $ dw close".bold.blue
-        puts "to mark this task completed."
-        puts hrb
-      elsif @waiting.size > 0
-        puts "You can choose to switch those branches, or press enter to leave"
-        print @waiting.keys.join(", ").blue + ":"
-        ans = STDIN.gets.chomp!
-        # if @waiting[ans]
-      else
-        puts hrb
-      end
-    end
-
     def process!
       self.hello
 
@@ -78,7 +36,7 @@ module DevFlow
             # if the task not started yet, update progress
             upload_progress!(@waiting[ans.to_i], 10) unless @waiting[ans.to_i].progress > 0
           else
-            error "Invalid input #{ans}. Can not continue." if ans
+            error "Invalid input #{ans}. Can not continue." if ans and ans.size > 0
           end
         end
       else # if the wd is not clean
