af198fa51ac2c86e8e81dbebc1a57adfb9761ac8
Huang Wei <huangw@7lime.com>
2014-09-05 10:06:02 +0800

    start rewrite

--- lib/dev_flow/version.rb
+++
@@ -1,3 +0,0 @@
-module DevFlow
-  VERSION = '0.6.1'
-end
--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rspec'
-require 'dev_flow'
-require "term/ansicolor"
-class String; include Term::ANSIColor end
-
-RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter     = 'documentation'
-end
